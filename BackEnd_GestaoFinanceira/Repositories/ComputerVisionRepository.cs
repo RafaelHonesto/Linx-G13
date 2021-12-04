70ae2c9673a33a546dacc888eb4ea014ee571963
Lucas <lucasimionato1910@hotmail.com>
2021-12-04 00:10:43 -0300

    publish

--- BackEnd_GestaoFinanceira/Repositories/ComputerVisionRepository.cs
+++ BackEnd_GestaoFinanceira/Repositories/ComputerVisionRepository.cs
@@ -19,12 +19,7 @@ namespace BackEnd_GestaoFinanceira.Repositories
         // </snippet_vars>
 	// </snippet_using_and_vars>
 
-        // Download these images (link in prerequisites), or you can use any appropriate image on your local machine.
-        private const string READ_TEXT_LOCAL_IMAGE = "printed_text.jpg";
 
-        // <snippet_readtext_url>
-        private const string READ_TEXT_URL_IMAGE = "https://intelligentkioskstore.blob.core.windows.net/visionapi/suggestedphotos/3.png";
-        // </snippet_readtext_url>
 
         public ReadOperationResult ReadFile(string url)
         {
@@ -40,7 +35,7 @@ namespace BackEnd_GestaoFinanceira.Repositories
          * AUTHENTICATE
          * Creates a Computer Vision client used by each example.
          */
-        public static ComputerVisionClient Authenticate(string endpoint, string key)
+        private static ComputerVisionClient Authenticate(string endpoint, string key)
         {
             ComputerVisionClient client =
               new ComputerVisionClient(new ApiKeyServiceClientCredentials(key))
@@ -57,7 +52,7 @@ namespace BackEnd_GestaoFinanceira.Repositories
          * READ FILE - URL 
          * Extracts text. 
          */
-        public static async Task<ReadOperationResult> ReadFileUrl(ComputerVisionClient client, string urlFile)
+        private static async Task<ReadOperationResult> ReadFileUrl(ComputerVisionClient client, string urlFile)
         {
             // Read text from URL
             var textHeaders = await client.ReadAsync(urlFile);
@@ -83,61 +78,6 @@ namespace BackEnd_GestaoFinanceira.Repositories
             // </snippet_readfileurl_2>
 
             return results;
-        }
-        // </snippet_readfileurl_3>
-
-        /*
-         * END - READ FILE - URL
-         */
-
-
-        // <snippet_read_local>
-        /*
-         * READ FILE - LOCAL
-         */
-
-        public static async Task ReadFileLocal(ComputerVisionClient client, string localFile)
-        {
-            Console.WriteLine("----------------------------------------------------------");
-            Console.WriteLine("READ FILE FROM LOCAL");
-            Console.WriteLine();
-
-            // Read text from URL
-            var textHeaders = await client.ReadInStreamAsync(File.OpenRead(localFile));
-            // After the request, get the operation location (operation ID)
-            string operationLocation = textHeaders.OperationLocation;
-            Thread.Sleep(2000);
-
-            // <snippet_extract_response>
-            // Retrieve the URI where the recognized text will be stored from the Operation-Location header.
-            // We only need the ID and not the full URL
-            const int numberOfCharsInOperationId = 36;
-            string operationId = operationLocation.Substring(operationLocation.Length - numberOfCharsInOperationId);
-
-            // Extract the text
-            ReadOperationResult results;
-            Console.WriteLine($"Reading text from local file {Path.GetFileName(localFile)}...");
-            Console.WriteLine();
-            do
-            {
-                results = await client.GetReadResultAsync(Guid.Parse(operationId));
-            }
-            while ((results.Status == OperationStatusCodes.Running ||
-                results.Status == OperationStatusCodes.NotStarted));
-            // </snippet_extract_response>
-
-            // <snippet_extract_display>
-            // Display the found text.
-            Console.WriteLine();
-            var textUrlFileResults = results.AnalyzeResult.ReadResults;
-            foreach (ReadResult page in textUrlFileResults)
-            {
-                foreach (Line line in page.Lines)
-                {
-                    Console.WriteLine(line.Text);
-                }
-            }
-            Console.WriteLine();
         }
     }
 }
